3351ffd82b68408c16ab55b4661e7863db85ed74
tolaked <adetolaakere1@gmail.com>
2019-03-08 09:02:46 +0000

    Updates

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,115 +1,90 @@
-const composeBox = document.querySelector(".compose-box");
-const inboxMessage = document.querySelector(".inbox-list");
-const sentMessage = document.querySelector(".sent-list");
-const draftMessage = document.querySelector(".draft-list");
-const groupMenu = document.querySelector("group-content");
-const inboxButton = document.querySelector(".inbox");
-const sentButton = document.querySelector(".sent");
-const draftButton = document.querySelector(".draft");
-const groupButton = document.querySelector(".groups");
-const readEmail = document.querySelector(".main-new-inbox-body");
-const inboxMessageContent = document.querySelector("inbox-messageContent");
-const composeButton = document.querySelector(".compose");
-const closeIcon = document.querySelector(".close");
-const header = document.querySelector(".header");
-const mainBody = document.querySelector(".main-body");
-const deleteIcon = document.querySelector(".delete-icon");
-const createGroupOverlay = document.querySelector(".create-group-form");
-const createGroup = document.querySelector(".create-group");
-const cancelForm = document.querySelector(".cancel-message-wrapper");
-const dropdownContent = document.querySelector(".dropdownContent");
-
-document.querySelector(".time-received").textContent = new Date().toUTCString();
-
-/* When user clicks on the button, it hides and shows the content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+const showMessageContent = (evt) => {
+  if(evt.target.classList.contains('messages') || evt.target.classList.contains('message-details')) {
+    const messageContainer = document.querySelectorAll('.message-container');
+    messageContainer.forEach(currentElement => {
+      currentElement.style.display = 'none'
+    });
+    document.querySelector('.main-new-inbox-body').style.display = 'block';
+  }
 }
 
-cancelForm.addEventListener("click", () => {
-  createGroupOverlay.style.display = "none";
-});
+const showListContent = (clickedElement) => {
 
-createGroup.addEventListener("click", () => {
-  createGroupOverlay.style.display = "block";
-});
-closeIcon.addEventListener("click", () => {
-  composeBox.style.display = "none";
-  inboxMessage.style.display = "block";
-});
-
-inboxMessage.addEventListener("click", () => {
-  readEmail.style.display = "block";
-  inboxMessage.style.display = "none";
-  header.style.display = "none";
-  mainBody.style.display = "none";
-  draftMessage.style.display = "none";
-});
-
-sentButton.addEventListener("click", () => {
-  sentMessage.style.display = "block";
-  inboxMessage.style.display = "none";
-  draftMessage.style.display = "none";
-
-  readEmail.style.display = "none";
-  composeBox.style.display = "none";
-});
-
-inboxButton.addEventListener("click", () => {
-  sentMessage.style.display = "none";
-  inboxMessage.style.display = "block";
-  draftMessage.style.display = "none";
-
-  readEmail.style.display = "none";
-  composeBox.style.display = "none";
-});
-
-draftButton.addEventListener("click", () => {
-  sentMessage.style.display = "none";
-  inboxMessage.style.display = "none";
-  draftMessage.style.display = "block";
-  // groupMenu.style.display = "none";
-  readEmail.style.display = "none";
-  composeBox.style.display = "none";
-});
-
-composeButton.addEventListener("click", () => {
-  composeBox.style.display = "block";
-  sentMessage.style.display = "none";
-  inboxMessage.style.display = "none";
-  draftMessage.style.display = "none";
-});
-document.addEventListener("click", e => {
-  if (e.target.className == "close") {
-    composeBox.style.display = "none";
+  // hides sidebar if on responsive mode
+  if(window.innerWidth <= 600) {
+    sidebar.classList.toggle('show-sidebar');
   }
 
-  if (e.target.classList[0] == "inbox") {
-    draftButton.classList.remove("active");
-    sentButton.classList.remove("active");
-    inboxButton.classList.add("active");
-    groupButton.classList.remove("active");
-  }
-  if (e.target.classList[0] == "sent") {
-    draftButton.classList.remove("active");
-    sentButton.classList.add("active");
-    inboxButton.classList.remove("active");
-    groupButton.classList.remove("active");
-  }
-  if (e.target.classList[0] == "draft") {
-    draftButton.classList.add("active");
-    sentButton.classList.remove("active");
-    inboxButton.classList.remove("active");
-    groupButton.classList.remove("active");
-  }
-  if (e.target.classList[0] == "groups") {
-    draftButton.classList.remove("active");
-    sentButton.classList.remove("active");
-    inboxButton.classList.remove("active");
-    groupButton.classList.add("active");
-  }
-});
+  const allActiveTab = document.querySelectorAll('.active');
+  allActiveTab.forEach(currentTab => {
+    currentTab.classList.remove('active');
+  });
 
-window.onload = () => {
-  inboxButton.classList.add("active");
+  const allChildren = mainContent.children;
+  const childrenArray = Array.prototype.slice.call(allChildren);
+  childrenArray.forEach(currentElement => {
+    currentElement.style.display = 'none';
+
+    if (currentElement.classList.contains(`${clickedElement}-container`)) {
+      currentElement.firstElementChild.style.display = 'block';
+      document.querySelector(`.${clickedElement}`).classList.add('active');
+      currentElement.style.display = 'block';
+    }
+  });
 };
+
+const displaMainContent = (evt) => {
+  const currentlyClicked = evt.target.className;
+  if (currentlyClicked === 'inbox') {
+    showListContent(currentlyClicked);
+  }
+
+  if (currentlyClicked === 'sent') {
+    showListContent(currentlyClicked);
+  }
+
+  if (currentlyClicked === 'draft') {
+    showListContent(currentlyClicked);
+  }
+
+  if (currentlyClicked === 'compose') {
+    const close = document.querySelector('.close');
+    close.addEventListener('click', () => {
+      document.querySelector('.inbox-container').style.display = 'block';
+      document.querySelector('.compose-container').style.display = 'none';
+    });
+    showListContent(currentlyClicked);
+  }
+
+  if (currentlyClicked === 'groups') {
+    const groupsContent = document.querySelector('.groups-dropdown-content');
+    groupsContent.classList.toggle('show-dropdown-content');
+  }
+
+  if (currentlyClicked === 'create-group') {
+    const createForm = (document.querySelector('.create-group-form').style.display = 'block');
+  }
+};
+
+const showMobileMenu = (evt) => {
+  evt.preventDefault();
+  sidebar.classList.toggle('show-sidebar');
+}
+
+const createGroupForm = document.querySelector('.create-group-form');
+const sidebar = document.querySelector('.sidebar');
+const mainContent = document.querySelector('.content');
+const menuIcon = document.querySelector('.menu-icon');
+
+mainContent.addEventListener('click', showMessageContent);
+
+sidebar.addEventListener('click', displaMainContent);
+
+menuIcon.addEventListener('click', showMobileMenu);
+
+createGroupForm.addEventListener('click', (evt) => {
+  if (evt.target === createGroupForm || evt.target.className === 'cancel-form') {
+    createGroupForm.style.display = 'none';
+  }
+});
+document.querySelector(".time-received").textContent = new Date().toUTCString();
